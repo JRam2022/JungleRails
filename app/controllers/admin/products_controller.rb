d92bc51e113256d807160843f26135c797801bd6
JRam2022 <jasonram778@gmail.com>
2022-06-01 23:16:39 -0700

    Add HTTP auth for admin pages

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,14 +1,18 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
+    
     @products = Product.order(id: :desc).all
   end
 
   def new
+    
     @product = Product.new
   end
 
   def create
+    
     @product = Product.new(product_params)
 
     if @product.save
@@ -19,6 +23,7 @@ class Admin::ProductsController < ApplicationController
   end
 
   def destroy
+    
     @product = Product.find params[:id]
     @product.destroy
     redirect_to [:admin, :products], notice: 'Product deleted!'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
   protect_from_forgery with: :exception
 
   private
@@ -29,4 +28,5 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
 end
