6695296bbe5da46bb4f0c8c448539180ebe2fc3b
JRam2022 <jasonram778@gmail.com>
2022-06-02 19:16:11 -0700

    Create Category table at categories index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,16 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
   def index
+    @categories = Category.all
   end
 
   def new
+    
   end
 
   def create
+
+
   end
 end
