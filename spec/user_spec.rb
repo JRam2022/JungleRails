91950da9f7c805071fbc413aae5303e10ed43447
JRam2022 <jasonram778@gmail.com>
2022-06-07 19:29:28 -0700

    Create authenticate credentials method in User model and rspec tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     context 'Given all fields, email is unique AND password is minumum length' do
       it 'Passes validation' do
       user = User.new(first_name:'J', last_name:'R', email:'test@test.com', password:'12', password_confirmation:'12')
-      expect(user).to be_valid
+        expect(user).to be_valid
       puts user.errors.full_messages
       end
     end
@@ -14,17 +14,37 @@ RSpec.describe User, type: :model do
       it 'Fails validation' do
       user1 = User.create(first_name:'J', last_name:'R', email:'test@test.com', password:'12', password_confirmation:'12')
       user2 = User.create(first_name:'Jay', last_name:'Ram', email:'tesT@test.com', password:'12', password_confirmation:'12')
-      expect(user2).not_to be_valid
+        expect(user2).not_to be_valid
       puts user2.errors.full_messages
       end
     end
 
     context 'Given a password UNDER minimum length' do
       it 'Fails validation' do
-      user = User.new(first_name: 'JJ', last_name:'RR', email:'test2@test.com', password:'1', password_confirmation:'1')
-      expect(user).not_to be_valid
+      user = User.new(first_name:'JJ', last_name:'RR', email:'test2@test.com', password:'1', password_confirmation:'1')
+        expect(user).not_to be_valid
       puts user.errors.full_messages
       end
     end
+
+    describe '.authenticate_with_credentials' do
+      context 'checks if user is able to login' do
+        it 'return user when matches' do
+          userCreate = User.create(first_name:'J', last_name:'R', email:'test@test.com', password:'12', password_confirmation:'12')
+          user = User.authenticate_with_credentials('test@test.com', '12')
+        expect(user).to eql(userCreate)
+        puts user.errors.full_messages
+        end
+      end
+
+      context 'Checks if user does not match' do
+        it 'return nil when no match' do
+          userCreate = User.create(first_name:'J', last_name:'R', email:'test@test.com', password:'12', password_confirmation:'12')
+          user = User.authenticate_with_credentials('testt@test.com', '12')
+        expect(user).not_to eql(userCreate)
+        end
+      end
+      
+    end 
   end
 end
